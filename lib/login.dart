e1f03b88403733ea41fc1efc8fcc8970c06a8d6f
Saul <saule@umes.edu.gt>
2022-07-29 19:13:22 -0600

    Layout de botones listo

--- lib/login.dart
+++ lib/login.dart
@@ -10,31 +10,129 @@ class LoginScreen extends StatelessWidget {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                height: 56,
-                width: 263,
-                child: const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Usuario',
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    height: 56,
+                    width: 263,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Usuario',
+                      ),
+                    ),
                   ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                    height: 56,
+                    width: 263,
+                    child: const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  const Text(
+                    'Olvidaste tu contraseña?',
+                    style: TextStyle(
+                      color: Color(0xFF09ADEA),
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 55.0,
+              ),
+              const Text(
+                'Ingresa con:',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
               const SizedBox(
-                height: 30.0,
+                height: 25.0,
               ),
               Container(
-                height: 56,
-                width: 263,
-                child: const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
+                width: 263.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Ink(
+                      width: 54.0,
+                      height: 54.0,
+                      decoration: const ShapeDecoration(
+                        color: Color(0xFF1877F2),
+                        shape: CircleBorder(),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.facebook),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                    Ink(
+                      width: 54.0,
+                      height: 54.0,
+                      decoration: const ShapeDecoration(
+                        color: Colors.black12,
+                        shape: CircleBorder(),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.headphones),
+                        color: Colors.black,
+                        onPressed: () {},
+                      ),
+                    ),
+                    Ink(
+                      width: 54.0,
+                      height: 54.0,
+                      decoration: const ShapeDecoration(
+                        color: Colors.black,
+                        shape: CircleBorder(),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.apple),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
                 ),
               ),
+              const SizedBox(
+                height: 82.0,
+              ),
+              SizedBox(
+                height: 40.0,
+                width: 263.0,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(const Color(0xFF6750A4)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100.0),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Iniciar Sesión'),
+                ),
+              )
             ],
           ),
         ),
